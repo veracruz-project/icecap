d0fbaf6d5660fc1263dd45ca0a74ccecf9ab1b06
Nick Spinale <nick@nickspinale.com>
2021-02-16 18:03:33 +0000

    nix: Add meta build test

---
+++ hack/build-test.nix
@@ -0,0 +1,20 @@
+let
+  top = import ../nix;
+  inherit (top) lib dev linux instances;
+  inherit (top.none.icecap) icecapPlats;
+in
+  lib.concatMap (plat: [
+    instances.${plat}.test.sel4test.run
+    instances.${plat}.test.host.run
+    instances.${plat}.test.host-and-adjacent-vm.run
+    instances.${plat}.test.timer-and-serial.run
+    instances.${plat}.test.timer-and-serial-from-realm.run
+    instances.${plat}.test.mirage.run
+    instances.${plat}.demos.minimal.run
+    instances.${plat}.demos.minimal-root.run
+    instances.${plat}.demos.realm-vm.run
+    instances.${plat}.bench.baseline.run
+    instances.${plat}.bench.baseline.test
+  ]) icecapPlats ++ lib.concatMap (host: [
+    host.icecap._9p-server
+  ]) [ dev linux ]
