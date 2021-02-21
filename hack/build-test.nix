5504952f458b35b724bb26e311abf2a4a64d1946
Nick Spinale <nick@nickspinale.com>
2021-02-21 05:49:22 +0000

    hack: Update build-test.nix

--- hack/build-test.nix
+++ hack/build-test.nix
@@ -9,10 +9,10 @@ in
     instances.${plat}.test.host-and-adjacent-vm.run
     instances.${plat}.test.timer-and-serial.run
     instances.${plat}.test.timer-and-serial-from-realm.run
-    instances.${plat}.test.mirage.run
     instances.${plat}.demos.minimal.run
     instances.${plat}.demos.minimal-root.run
     instances.${plat}.demos.realm-vm.run
+    instances.${plat}.demos.mirage.run
     instances.${plat}.bench.baseline.run
     instances.${plat}.bench.baseline.test
   ]) icecapPlats ++ lib.concatMap (host: [
