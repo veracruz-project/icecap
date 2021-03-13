faa7bea83da735c0abc936279d1fa359bd46c3ae
Nick Spinale <nick@nickspinale.com>
2021-03-13 22:36:50 +0000

    linux: Progress minimizing guest kconfig

--- nix/scope/linux-kernel/guest/default.nix
+++ nix/scope/linux-kernel/guest/default.nix
@@ -8,18 +8,10 @@ let
 
   source = linuxKernelUnifiedSource;
 
-  # CONFIG_ICECAP=y
-  # CONFIG_TMPFS_POSIX_ACL=y
-  # CONFIG_CRYPTO_USER_API_HASH=m
-  # CONFIG_NETFILTER_XT_MATCH_BPF=m
-  # CONFIG_IP_NF_IPTABLES=y
-
-  # CONFIG_NF_CONNTRACK_FTP
-  # CONFIG_NF_NAT_FTP
-  # ...
-
   # TODO
-  # CONFIG_LOCALVERSION_AUTO=n
+  # y crypto acceleration for net stack
+  # n debug symbols
+  # expert: n multiuser, n block
 
   config = makeConfig {
     inherit source;
