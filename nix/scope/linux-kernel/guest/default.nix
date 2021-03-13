97578684371f098fc1e5448bebf4f0ed9fcfcecb
Nick Spinale <nick@nickspinale.com>
2021-03-13 21:26:35 +0000

    linux: Begin minification of guest kconfig

--- nix/scope/linux-kernel/guest/default.nix
+++ nix/scope/linux-kernel/guest/default.nix
@@ -1,2 +1,36 @@
-# HACK should be minimized
-import ../host/virt
+{ lib, fetchgit, linux-ng
+, linuxKernelUnifiedSource
+}:
+
+with linux-ng;
+
+let
+
+  source = linuxKernelUnifiedSource;
+
+  # CONFIG_ICECAP=y
+  # CONFIG_TMPFS_POSIX_ACL=y
+  # CONFIG_CRYPTO_USER_API_HASH=m
+  # CONFIG_NETFILTER_XT_MATCH_BPF=m
+  # CONFIG_IP_NF_IPTABLES=y
+
+  # CONFIG_NF_CONNTRACK_FTP
+  # CONFIG_NF_NAT_FTP
+  # ...
+
+  # TODO
+  # CONFIG_LOCALVERSION_AUTO=n
+
+  config = makeConfig {
+    inherit source;
+    target = "alldefconfig";
+    allconfig = ./defconfig;
+  };
+
+in
+doKernel rec {
+  inherit source config;
+  # modules = true;
+  modules = false;
+  dtbs = true;
+}
