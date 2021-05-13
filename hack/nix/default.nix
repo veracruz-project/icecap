a31291deb7d34b3aa091e1c436de92a96acbfcfe
Nick Spinale <nick@nickspinale.com>
2021-05-13 12:24:19 +0000

    hack: Bump nix

--- hack/nix/default.nix
+++ hack/nix/default.nix
@@ -1,10 +1,10 @@
 let
   system = builtins.currentSystem;
-
+ 
   src = builtins.fetchGit rec {
     url = "https://gitlab.com/arm-research/security/icecap/nix.git";
     ref = "refs/tags/icecap/keep/${builtins.substring 0 32 rev}";
-    rev = "550e11f077ae508abde5a33998a9d4029880e7b2";
+    rev = "f4a9fb67da5bc55221be451556710dfacb39eda0";
   };
 
   nix = import src;
