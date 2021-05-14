b93862d3922eececed17a2a77c47f1a5df79f727
Nick Spinale <nick@nickspinale.com>
2021-05-14 13:28:28 +0000

    rust: Increase generality of generated-module-hack crate

--- src/rust/helpers/generated-module-hack/src/lib.rs
+++ src/rust/helpers/generated-module-hack/src/lib.rs
@@ -16,11 +16,11 @@ use syn::{parse, ItemMod};
 /// ```
 #[proc_macro_attribute]
 pub fn generated_module(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let ident = parse::<ItemMod>(item).unwrap().ident;
-    let path = Path::new(&env::var("OUT_DIR").unwrap()).join(ident.to_string()).with_extension("rs")
+    let item = parse::<ItemMod>(item).unwrap();
+    let path = Path::new(&env::var("OUT_DIR").unwrap()).join(item.ident.to_string()).with_extension("rs")
         .to_str().unwrap().to_string();
     quote!(
         #[path = #path]
-        mod #ident;
+        #item
     ).into()
 }
