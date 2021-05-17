dc61a06c54702523930eda6589d88fa90bb6c52f
Nick Spinale <nick@nickspinale.com>
2021-05-17 13:11:34 +0000

    nixos-lite: Add DHCP support

--- nix/nixos-lite/modules/1-stage/net.nix
+++ nix/nixos-lite/modules/1-stage/net.nix
@@ -5,6 +5,32 @@ with lib;
 let
   cfg = config.net;
 
+  udhcpcScript = pkgs.writeScript "udhcpc.sh" ''
+    #!${config.build.extraUtils}/bin/sh
+    if [ "$1" = bound ]; then
+      ip address add "$ip/$mask" dev "$interface"
+      if [ -n "$mtu" ]; then
+        ip link set mtu "$mtu" dev "$interface"
+      fi
+      if [ -n "$staticroutes" ]; then
+        echo "$staticroutes" \
+          | sed -r "s@(\S+) (\S+)@ ip route add \"\1\" via \"\2\" dev \"$interface\" ; @g" \
+          | sed -r "s@ via \"0\.0\.0\.0\"@@g" \
+          | /bin/sh
+      fi
+      if [ -n "$router" ]; then
+        ip route add "$router" dev "$interface" # just in case if "$router" is not within "$ip/$mask" (e.g. Hetzner Cloud)
+        ip route add default via "$router" dev "$interface"
+      fi
+      if [ -n "$dns" ]; then
+        rm -f /etc/resolv.conf
+        for i in $dns; do
+          echo "nameserver $dns" >> /etc/resolv.conf
+        done
+      fi
+    fi
+  '';
+
 in {
   options = {
 
@@ -20,9 +46,11 @@ in {
     initramfs.extraInitCommands = ''
       ${concatStrings (mapAttrsToList (interface: v: ''
         echo "setting up ${interface}..."
-        ${optionalString (hasAttr "static" v) ''
-          ip link set ${interface} up
+        ip link set ${interface} up
+        ${if hasAttr "static" v then ''
           ip address add ${v.static} dev ${interface}
+        '' else ''
+          udhcpc --quit --now -i ${interface} -O staticroutes --script ${udhcpcScript}
         ''}
       '') cfg.interfaces)}
     '';
--- nix/nixos-lite/modules/2-stage/net.nix
+++ nix/nixos-lite/modules/2-stage/net.nix
@@ -6,6 +6,32 @@ let
   cfgInitramfs = config.initramfs.net;
   cfg = config.net;
 
+  udhcpcScript = sh: pkgs.writeScript "udhcpc.sh" ''
+    #!${sh}
+    if [ "$1" = bound ]; then
+      ip address add "$ip/$mask" dev "$interface"
+      if [ -n "$mtu" ]; then
+        ip link set mtu "$mtu" dev "$interface"
+      fi
+      if [ -n "$staticroutes" ]; then
+        echo "$staticroutes" \
+          | sed -r "s@(\S+) (\S+)@ ip route add \"\1\" via \"\2\" dev \"$interface\" ; @g" \
+          | sed -r "s@ via \"0\.0\.0\.0\"@@g" \
+          | /bin/sh
+      fi
+      if [ -n "$router" ]; then
+        ip route add "$router" dev "$interface" # just in case if "$router" is not within "$ip/$mask" (e.g. Hetzner Cloud)
+        ip route add default via "$router" dev "$interface"
+      fi
+      if [ -n "$dns" ]; then
+        rm -f /etc/resolv.conf
+        for i in $dns; do
+          echo "nameserver $dns" >> /etc/resolv.conf
+        done
+      fi
+    fi
+  '';
+
 in {
   options = {
 
@@ -26,9 +52,11 @@ in {
     initramfs.mntCommands = ''
       ${concatStrings (mapAttrsToList (interface: v: ''
         echo "setting up ${interface}..."
-        ${optionalString (hasAttr "static" v) ''
-          ip link set ${interface} up
+        ip link set ${interface} up
+        ${if hasAttr "static" v then ''
           ip address add ${v.static} dev ${interface}
+        '' else ''
+          udhcpc --quit --now -i ${interface} -O staticroutes --script ${udhcpcScript "${config.build.extraUtils}/bin/sh"}
         ''}
       '') cfgInitramfs.interfaces)}
     '';
@@ -36,9 +64,11 @@ in {
     initramfs.extraNextInit = ''
       ${concatStrings (mapAttrsToList (interface: v: ''
         echo "setting up ${interface}..."
-        ${optionalString (hasAttr "static" v) ''
-          ip link set ${interface} up
+        ip link set ${interface} up
+        ${if hasAttr "static" v then ''
           ip address add ${v.static} dev ${interface}
+        '' else ''
+          udhcpc --quit --now -i ${interface} -O staticroutes --script ${udhcpcScript "/bin/sh"}
         ''}
       '') cfg.interfaces)}
     '';
