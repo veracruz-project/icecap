1529ffb82512ac8553e062c32067bb1b1445569b
Nick Spinale <nick@nickspinale.com>
2021-06-22 17:31:46 +0100

    docker: Add stateful image

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,8 +2,8 @@ set -e
 
 if [ ! -f /nix/.installed ]; then
     echo "Installing Nix..."
-    curl -L https://raw.githubusercontent.com/nspin/minimally-invasive-nix-installer/dist/install.sh -o install-nix.sh
-    echo "ebad4f63eb3df7807e249cc3e3883f6d38e28303983f9892dde71f944c0a3558 install-nix.sh" | sha256sum -c -
+    curl -L https://github.com/nspin/minimally-invasive-nix-installer/raw/dist-11p9fcf3ca/dist/install.sh -o install-nix.sh
+    echo "8bf39c7fc93534ee98fdbf2c3ae970def38ce0eb2f2146a66b067bc3975f0cda install-nix.sh" | sha256sum -c -
     bash install-nix.sh
     rm install-nix.sh
     touch /nix/.installed
