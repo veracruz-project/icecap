78e9b321601dc033450184b723173117280dd1ba
Nick Spinale <nick@nickspinale.com>
2021-08-28 15:04:10 +0000

    nix: trivial: Simplify

--- nix/scope/configured/sel4-kernel/object-sizes.nix
+++ nix/scope/configured/sel4-kernel/object-sizes.nix
@@ -1,8 +1,7 @@
 { runCommandCC, repos, libsel4, libs }:
 
-# TODO is this sound?
 runCommandCC "object_sizes.yaml" {
   buildInputs = [ libsel4 libs.icecap-autoconf ];
 } ''
-  grep -v autoconf.h ${repos.rel.capdl "object_sizes/object_sizes.yaml"} | $CC -E -P - > $out
+  $CC -E -P - < ${repos.rel.capdl "object_sizes/object_sizes.yaml"} > $out
 ''
