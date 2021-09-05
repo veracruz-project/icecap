d7972db9a850b1b4dd614e22e32cbe47250953d4
Nick Spinale <nick@nickspinale.com>
2021-09-05 03:45:36 +0000

    meta: Clean up mkInstance

--- nix/meta/default.nix
+++ nix/meta/default.nix
@@ -2,9 +2,17 @@
 
 rec {
 
-  demos = lib.mapAttrs (_: path: import path { inherit lib pkgs; }) (import ./demos);
+  demos = lib.flip lib.mapAttrs (import ./demos) (_: path:
+    import path { inherit lib pkgs; }
+  );
 
-  tests = import ./tests { inherit lib pkgs; };
+  tests = lib.flip lib.mapAttrs (import ./tests) (_: path:
+    lib.flip lib.mapAttrs pkgs.none.icecap.configured (_: configured:
+      configured.callPackage path {
+        mkInstance = mkInstance configured;
+      }
+    )
+  );
 
   buildTest = import ./build-test.nix {
     inherit lib pkgs meta;
@@ -13,5 +21,7 @@ rec {
   tcbSize = import ./tcb-size.nix {
     inherit pkgs;
   };
+
+  mkInstance = import ./mk-instance.nix { inherit lib pkgs; };
 
 }
--- nix/meta/tests/default.nix
+++ nix/meta/tests/default.nix
@@ -1,54 +1,5 @@
-{ lib, pkgs }:
-
-let
-
-  mkInstance = configured:
-
-    let
-      inherit (configured) icecapPlat;
-    in
-
-    f: lib.fix (self:
-
-      let
-        attrs = f self;
-      in
-
-      with self; {
-
-        inherit configured;
-
-        composition = configured.icecapFirmware;
-        payload = {};
-        allDebugFiles = true;
-        extraLinks = {};
-        icecapPlatArgs = {};
-
-        run = pkgs.none.icecap.platUtils.${icecapPlat}.bundle {
-          firmware = composition.image;
-          inherit payload;
-          platArgs = icecapPlatArgs.${icecapPlat} or {};
-          extraLinks = lib.mapAttrs' (k: lib.nameValuePair "debug/${k}") ({
-              icecap-show-backtrace = "${pkgs.dev.icecap.icecap-show-backtrace}/bin/show-backtrace";
-            } // composition.debugFiles // lib.optionalAttrs allDebugFiles composition.cdlDebugFiles
-          ) // extraLinks;
-        };
-
-      } // attrs
-    );
-
-  tests = {
-    realm-vm = ./realm-vm;
-    firecracker = ./firecracker;
-    analysis = ./analysis;
-  };
-
-in
-
-lib.flip lib.mapAttrs tests (_: path:
-  lib.flip lib.mapAttrs pkgs.none.icecap.configured (_: configured:
-    configured.callPackage path {
-      mkInstance = mkInstance configured;
-    }
-  )
-)
+{
+  realm-vm = ./realm-vm;
+  firecracker = ./firecracker;
+  analysis = ./analysis;
+}
