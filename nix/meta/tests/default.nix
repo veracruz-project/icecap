110febb21d1dbb17d92e4d1a87ac0760a91dc4e8
Nick Spinale <nick@nickspinale.com>
2021-09-05 02:17:34 +0000

    meta/tests: Clean up

--- nix/meta/tests/default.nix
+++ nix/meta/tests/default.nix
@@ -6,8 +6,6 @@ let
   
     let
       inherit (configured) icecapPlat;
-      inherit (pkgs.none.icecap) platUtils;
-      inherit (pkgs.none.buildPackages.icecap) icecap-show-backtrace;
     in
 
     f: lib.fix (self:
@@ -26,12 +24,12 @@ let
         extraLinks = {};
         icecapPlatArgs = {};
 
-        run = platUtils.${icecapPlat}.bundle {
+        run = pkgs.none.icecap.platUtils.${icecapPlat}.bundle {
           firmware = composition.image;
           inherit payload;
           platArgs = icecapPlatArgs.${icecapPlat} or {};
           extraLinks = lib.mapAttrs' (k: lib.nameValuePair "debug/${k}") ({
-              icecap-show-backtrace = "${icecap-show-backtrace}/bin/show-backtrace";
+              icecap-show-backtrace = "${pkgs.dev.icecap.icecap-show-backtrace}/bin/show-backtrace";
             } // composition.debugFiles // lib.optionalAttrs allDebugFiles composition.cdlDebugFiles
           ) // extraLinks;
         };
