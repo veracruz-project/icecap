f0ebaacb0882e0411df30f82dc65936d01bdeb0c
Nick Spinale <nick@nickspinale.com>
2021-09-05 01:06:37 +0000

    nixos-lite: Move Linux kernel module aggregation helpers to nix-linux

--- nix/nixos-lite/default.nix
+++ nix/nixos-lite/default.nix
@@ -7,6 +7,5 @@ self: with self;
 
   mkExtraUtils = callPackage ./pkgs/mk-extra-utils.nix {};
   mkNixInitramfs = callPackage ./pkgs/mk-nix-initramfs.nix {};
-  mkModulesClosure = callPackage ./pkgs/mk-modules-closure.nix {};
-  aggregateModules = callPackage ./pkgs/aggregate-modules.nix {};
+
 }
