4b93bcfdb47b18f5779c879c4100182de3bdefcf
Nick Spinale <nick@nickspinale.com>
2021-09-11 22:34:07 +0000

    tests: Don't run CPU overhead

--- nix/meta/tests/firecracker/realm.nix
+++ nix/meta/tests/firecracker/realm.nix
@@ -15,10 +15,10 @@ in
       echo "nameserver 1.1.1.1" > /etc/resolv.conf
       ip route add default via ${hostAddr} dev ${virtualIface}
 
-      for _ in $(seq 3); do
-        sysbench --test=cpu --cpu-max-prime=20000 --num-threads=1 run
-        sleep 5
-      done
+      # for _ in $(seq 3); do
+      #   sysbench --test=cpu --cpu-max-prime=20000 --num-threads=1 run
+      #   sleep 5
+      # done
 
       while true; do iperf3 -c ${hostAddr} && sleep 5 || break; done
 
