d80428638c10d092a3150603aa679acb5f8c74b1
Nick Spinale <nick@nickspinale.com>
2021-09-22 23:45:06 +0000

    firecracker: Add env

--- nix/scope/linux-user/firecracker/firecracker.nix
+++ nix/scope/linux-user/firecracker/firecracker.nix
@@ -3,8 +3,18 @@
 , buildRustPackage
 , fetchCrates
 , libfdt
+, mkShell, cargo, rustc, git, cacert, stdenv, buildPackages, crateUtils, nixToToml, python3
 }:
 
+let
+  # TODO why is this necessary?
+  fdtConfig = {
+    target.${hostPlatform.config}.fdt = {
+      rustc-link-search = [ "native=${libfdt}/lib" ];
+    };
+  };
+
+in
 buildRustPackage rec {
   pname = "firecracker";
   version = "0.25.0";
@@ -15,15 +25,44 @@ buildRustPackage rec {
 
   buildInputs = [ libfdt ];
 
-  extraCargoConfig = (fetchCrates "${src}/Cargo.lock").config // {
-    # TODO why is this necessary?
-    target.${hostPlatform.config}.fdt = {
-      rustc-link-search = [ "native=${libfdt}/lib" ];
-    };
-  };
+  extraCargoConfig = (fetchCrates "${src}/Cargo.lock").config // fdtConfig;
 
   postPatch = ''
     rm -r .cargo
   '';
+
+  passthru.env =
+    let
+      cargoConfig = nixToToml (crateUtils.clobber [
+        crateUtils.baseCargoConfig
+        fdtConfig
+      ]);
+    in
+      mkShell (crateUtils.baseEnv // {
+
+        depsBuildBuild = [ buildPackages.stdenv.cc ];
+        nativeBuildInputs = [ rustc cargo git cacert python3 ];
+        buildInputs = [ libfdt ];
+
+        shellHook = ''
+          c() {
+            rm -f .cargo/config
+            ln -s ${cargoConfig} .cargo/config
+          }
+          b() {
+            cargo build \
+              --target ${stdenv.hostPlatform.config} \
+              -j $NIX_BUILD_CORES
+          }
+          d() {
+            cargo doc \
+              --target ${stdenv.hostPlatform.config} \
+              -j $NIX_BUILD_CORES
+          }
+          s() {
+            (cd target/${stdenv.hostPlatform.config}/doc && python3 -m http.server)
+          }
+        '';
+      });
 
 }
