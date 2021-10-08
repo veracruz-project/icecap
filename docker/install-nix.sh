f169b3866803ca99801f0b10232e794191557524
Nick Spinale <nick@nickspinale.com>
2021-10-08 13:32:35 +0000

    docker: Bump Nix installer version

--- docker/install-nix.sh
+++ docker/install-nix.sh
@@ -1,16 +1,16 @@
 set -eu
 
 arch="$(uname -m)"
-version=dlp5yb3c19
+version=hwv38gkcw4
 script_name="install-${arch}-linux.sh"
 url="https://github.com/nspin/minimally-invasive-nix-installer/raw/dist-${version}/dist/${script_name}"
 
 case "$arch" in
     x86_64)
-        sha256=051153e5e106744ced7be1d80e176d7e225aba3ed9340a36242777d88c809ff7
+        sha256=828cb187045e69327b9164fae0374ec2ec64b8d5316b555dc1fa9a443111f94f
         ;;
     aarch64)
-        sha256=af9eebd54eb2a2eb552e774e2698d33f8e9e5dfde112153f0066e8e87278f9cf
+        sha256=b1e81a2cc8986cafd69baadd7d1999fa529dc30691eb253a6561f660957bfcb3
         ;;
     *)
         echo >&2 "unsupported architecture: '$arch'"
