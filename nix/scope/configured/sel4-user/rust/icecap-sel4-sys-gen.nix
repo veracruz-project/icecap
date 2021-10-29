b414ea94ce5c8ffa4a27ae1478634552f24a7373
Nick Spinale <nick@nickspinale.com>
2021-10-29 04:48:35 +0000

    nix: Improve style

--- nix/scope/configured/sel4-user/rust/icecap-sel4-sys-gen.nix
+++ nix/scope/configured/sel4-user/rust/icecap-sel4-sys-gen.nix
@@ -10,13 +10,15 @@ let
 
 in
 runCommandCC "icecap-gen.rs" {
+  LIBCLANG_PATH = "${lib.getLib buildPackages.llvmPackages.libclang}/lib";
   nativeBuildInputs = [
     bindgen rustfmt
   ];
-  LIBCLANG_PATH = "${lib.getLib buildPackages.llvmPackages.libclang}/lib";
   buildInputs = [
     libsel4 libs.icecap-autoconf
   ];
 } ''
-  bindgen ${wrapper} -o $out --use-core --ctypes-prefix=c_types --with-derive-default --rust-target nightly -- -target ${rustTargetName} $NIX_CFLAGS_COMPILE
+  bindgen ${wrapper} -o $out \
+    --use-core --ctypes-prefix=c_types --with-derive-default --rust-target nightly \
+    -- -target ${rustTargetName} $NIX_CFLAGS_COMPILE
 ''
