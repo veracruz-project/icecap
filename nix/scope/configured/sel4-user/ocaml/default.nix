bd3b6a195e55769c4f9f8dd1df73bc356a94d53d
Nick Spinale <nick@nickspinale.com>
2021-11-01 18:31:18 +0000

    nix: Style

--- nix/scope/configured/sel4-user/ocaml/default.nix
+++ nix/scope/configured/sel4-user/ocaml/default.nix
@@ -6,27 +6,27 @@
 {
   mkMirageBinary = mirageLibrary: buildIceCapComponent {
 
-      rootCrate = globalCrates.mirage;
+    rootCrate = globalCrates.mirage;
 
-      extraLastLayer = attrs: {
-        buildInputs = (attrs.buildInputs or []) ++ [
-          libs.icecap-pure # TODO
-          libs.icecap-mirage-glue
-          musl
-          mirageLibrary
-        ];
-        passthru = attrs.passthru // {
-          inherit mirageLibrary;
-        };
-
-        # HACK
-        # NOTE affects fingerprints, so causes last layer to build too much.
-        RUSTFLAGS = lib.concatMap (x: [ "-C" "link-arg=-l${x}" ]) [
-          "icecap-mirage-glue" "sel4asmrun" "mirage" "sel4asmrun" "icecap-mirage-glue" "c" "gcc"
-        ] ++ [
-          # TODO shouldn't be necessary
-          "-C" (let cc = stdenv.cc.cc; in "link-arg=-L${cc}/lib/gcc/${cc.targetConfig}/${cc.version}")
-        ];
+    extraLastLayer = attrs: {
+      buildInputs = (attrs.buildInputs or []) ++ [
+        libs.icecap-pure # TODO
+        libs.icecap-mirage-glue
+        musl
+        mirageLibrary
+      ];
+      passthru = attrs.passthru // {
+        inherit mirageLibrary;
       };
+
+      # HACK
+      # NOTE affects fingerprints, so causes last layer to build too much.
+      RUSTFLAGS = lib.concatMap (x: [ "-C" "link-arg=-l${x}" ]) [
+        "icecap-mirage-glue" "sel4asmrun" "mirage" "sel4asmrun" "icecap-mirage-glue" "c" "gcc"
+      ] ++ [
+        # TODO shouldn't be necessary
+        "-C" (let cc = stdenv.cc.cc; in "link-arg=-L${cc}/lib/gcc/${cc.targetConfig}/${cc.version}")
+      ];
     };
+  };
 }
