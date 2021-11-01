85a755a3ba58fff1ab1c34a05366c8beb32cdfa7
Nick Spinale <nick@nickspinale.com>
2021-11-01 13:03:16 +0000

    c: Move more into makefiles

--- nix/scope/configured/capdl/capdl-loader-lib.nix
+++ nix/scope/configured/capdl/capdl-loader-lib.nix
@@ -15,23 +15,16 @@ let
     cp -r ${kernel.patchedSource}/tools/hardware $out
   '';
 
-  platformInfo = linkFarm "platform-info" [
-    { name = "include/capdl_loader_app/platform_info.h";
-      path = runCommand "platform_info.h" {
-        nativeBuildInputs = [ python3 python3Packages.sel4-deps ];
-      } ''
-        ${py}/platform_sift.py --emit-c-syntax ${libsel4}/sel4-aux/platform_gen.yaml > $out
-      '';
-    }
-  ];
+  platformInfo = runCommand "platform_info.h" {
+    nativeBuildInputs = [ python3 python3Packages.sel4-deps ];
+  } ''
+    ${py}/platform_sift.py --emit-c-syntax ${libsel4}/sel4-aux/platform_gen.yaml > $out
+  '';
 
 in
 libs.mk {
   name = "capdl-loader-lib";
   root = icecapSrc.relativeSplit "c/boot/capdl-loader-core";
-  buildInputs = [
-    platformInfo
-  ];
   propagatedBuildInputs = [
     libsel4
     libs.icecap-runtime-root
@@ -40,35 +33,10 @@ libs.mk {
     libs.cpio
     libs.capdl-support-hack
   ];
-  extraCFlagsCompile = [
-    "-Wno-unused-variable"
-    "-Wno-unused-function"
-    "-Wno-unused-but-set-variable"
-    # TODO
-    # "-Wno-error=unused-variable"
-    # "-Wno-error=unused-function"
-    # "-Wno-error=unused-but-set-variable"
-  ];
   extra.CAPDL_LOADER_EXTERNAL_SOURCE = seL4EcosystemRepos.capdl.extendInnerSuffix "capdl-loader-app";
+  extra.CAPDL_LOADER_PLATFORM_INFO_H = platformInfo;
   extra.CAPDL_LOADER_CONFIG_IN_H = writeText "config_in.h" ''
     #pragma once
     #define CONFIG_CAPDL_LOADER_MAX_OBJECTS 10000
   '';
-  passthru = {
-    x = platformInfo;
-  };
 }
-
-# TODO
-# CapDLLoaderCallingConvention-STRINGS:INTERNAL=standard;registers
-# CapDLLoaderCallingConvention:STRING=standard
-# CapDLLoaderCallingConventionRegisters:INTERNAL=OFF
-# CapDLLoaderCallingConventionStandard:INTERNAL=ON
-# CapDLLoaderCallingConvention_all_strings:INTERNAL=standard;registers
-# CapDLLoaderFillsPerFrame:STRING=1
-# CapDLLoaderMaxObjects:STRING=90000
-# CapDLLoaderPrintCapDLObjects:BOOL=OFF
-# CapDLLoaderPrintDeviceInfo:BOOL=OFF
-# CapDLLoaderPrintUntypeds:BOOL=OFF
-# CapDLLoaderStaticAlloc:BOOL=OFF
-# CapDLLoaderWriteablePages:BOOL=OFF
--- src/c/boot/capdl-loader-core/icecap.mk
+++ src/c/boot/capdl-loader-core/icecap.mk
@@ -1,7 +1,17 @@
 libs += capdl_loader_core
 src-capdl_loader_core := $(CAPDL_LOADER_EXTERNAL_SOURCE)/src
 inc-capdl_loader_core := $(here)/include $(CAPDL_LOADER_EXTERNAL_SOURCE)/include
-gen-hdrs-capdl_loader_core += capdl_loader_app/config_in.h
+gen-hdrs-capdl_loader_core += \
+	capdl_loader_app/platform_info.h \
+	capdl_loader_app/config_in.h
+
+$(BUILD)/include/capdl_loader_app/platform_info.h: $(CAPDL_LOADER_PLATFORM_INFO_H)
+	install -D -T $< $@
 
 $(BUILD)/include/capdl_loader_app/config_in.h: $(CAPDL_LOADER_CONFIG_IN_H)
 	install -D -T $< $@
+
+# HACK
+CFLAGS += -Wno-unused-variable
+CFLAGS += -Wno-unused-function
+CFLAGS += -Wno-unused-but-set-variable
