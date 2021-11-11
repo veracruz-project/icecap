2c43271155026dd9bb48b902b5bbd3ec1bbdc27d
Nick Spinale <nick@nickspinale.com>
2021-11-11 16:22:24 +0000

    icedl: Expose constant

--- src/python/icedl/common/__init__.py
+++ src/python/icedl/common/__init__.py
@@ -1,6 +1,6 @@
 from icedl.common.composition import BaseComposition, RingBufferObjects, RingBufferSideObjects
 
 from icedl.common.components.base import BaseComponent
-from icedl.common.components.elf import ElfComponent, ElfThread
+from icedl.common.components.elf import ElfComponent, ElfThread, DEFAULT_PRIO
 from icedl.common.components.generic import GenericElfComponent
 from icedl.common.components.fault_handler import FaultHandler
