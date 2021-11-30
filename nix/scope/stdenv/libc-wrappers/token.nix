fb8c183408c44c15dc0a3634c9e2aea489cc2f38
Nick Spinale <nick@nickspinale.com>
2021-11-30 11:20:03 +0000

    nix: Extend token libc wrapper

--- nix/scope/stdenv/libc-wrappers/token.nix
+++ nix/scope/stdenv/libc-wrappers/token.nix
@@ -6,7 +6,11 @@ runCommandCC "libc" {} ''
   touch empty.s
   $AS empty.s -o empty.o
 
+  cp empty.o $out/lib/crt0.o
   cp empty.o $out/lib/crt1.o
   cp empty.o $out/lib/crti.o
   cp empty.o $out/lib/crtn.o
+
+  $AR r $out/lib/libc.a empty.o
+  $AR r $out/lib/libm.a empty.o
 ''
