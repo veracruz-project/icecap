d55c29a4c85c12d7249f55b8e86d34d6fba3cc3a
Nick Spinale <nick@nickspinale.com>
2021-12-05 15:16:07 +0000

    nix: Add note to cpio-utils

--- nix/scope/nix-utils/cpio-utils.nix
+++ nix/scope/nix-utils/cpio-utils.nix
@@ -2,6 +2,12 @@
 , icecapSrc
 }:
 
+# NOTE
+# GNU cpio with `-L -H newc` displays the following errant behavior:
+#   - incorrect ordering of files in the archive
+#   - incorrect handling of cases multiple links have the same target
+# As a workaround, we replace `-L` with a temporary directory and `cp -rL`.
+
 {
 
   # for when order matters
@@ -12,15 +18,15 @@
     in runCommand "archive.cpio" {
       nativeBuildInputs = [ cpio ];
     } ''
-      cp -rL ${links} links # HACK to handle case where multiple links have the same target
-      printf %s $'${txt}' | cpio -o -D links -L --reproducible -H newc > $out
+      cp -rL ${links} links # HACK see above
+      printf %s $'${txt}' | cpio -o -D links --reproducible -H newc > $out
     '';
 
   mkFrom = links: runCommand "archive.cpio" {
     nativeBuildInputs = [ cpio ];
   } ''
-    cp -rL ${links} links # HACK to handle case where multiple links have the same target
-    (cd links && find . -not -type d | cpio -o -L --reproducible -H newc > $out)
+    cp -rL ${links} links # HACK see above
+    (cd links && find . -not -type d | cpio -o --reproducible -H newc > $out)
   '';
 
   mkObj = { archive-cpio, symbolName }: runCommandCC "embedded-file.o" {} ''
