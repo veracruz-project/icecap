d5166ae34fb0830477d9fabcc856a6c27adac929
Nick Spinale <nick@nickspinale.com>
2022-01-04 10:15:04 +0000

    cache-maintenance: Replace scripts with Makefile

--- hack/cache-maintenance/populate.sh
+++
@@ -1,8 +0,0 @@
-set -eu
-
-here="$(dirname "$0")"
-host="$1"
-
-drv=$(nix-instantiate "$here/populate.nix")
-nix-store --realise $drv
-nix-copy-closure --include-outputs --to "$host" $drv
--- hack/cache-maintenance/source-test.sh
+++
@@ -1,7 +0,0 @@
-set -eu
-
-here="$(dirname "$0")"
-
-export CURRENT_REV="$(git show -s --format=%H)"
-
-nix-build "$here/source-test.nix" -A test
