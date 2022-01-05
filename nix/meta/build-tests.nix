040edf9f028bcb2e62bd77c3789685a87aebbe43
Nick Spinale <nick@nickspinale.com>
2022-01-05 03:01:27 +0000

    nix: Fix build tests

--- nix/meta/build-tests.nix
+++ nix/meta/build-tests.nix
@@ -3,24 +3,35 @@
 let
   inherit (pkgs) dev none linux musl;
 
-  allRoots = lib.flip lib.mapAttrsToList pkgs.none.icecap.configured (k: v: [
-    meta.demos.realm-vm.${k}.run
-    meta.tests.realm-vm.${k}.run
-    meta.tests.analysis.${k}.run
-    meta.tests.benchmark-utilisation.${k}.run
-    v.sysroot-rs
-  ] ++ lib.optionals dev.hostPlatform.isx86_64 [
-    meta.demos.realm-mirage.${k}.run
-  ]) ++ lib.flip lib.concatMap [ dev linux ] (host: [
-    host.icecap.crosvm-9p-server
-  ]) ++ [
+  allRoots = [
+
+    (lib.mapAttrsToList (_: lib.mapAttrsToList (_: plat: plat.run)) meta.demos)
+    (lib.mapAttrsToList (_: example: example.run) meta.examples)
+  
     dev.icecap.sel4-manual
-    meta.tests.firecracker.rpi4.boot
+
     musl.icecap.icecap-host
     musl.icecap.firecracker
     musl.icecap.firecracker-prebuilt
     musl.icecap.firectl
-  ] ++ lib.mapAttrsToList (_: example: example.run) meta.examples;
+
+    (lib.flip lib.concatMap [ dev linux ] (host: [
+      host.icecap.crosvm-9p-server
+    ]))
+
+    (lib.flip lib.mapAttrsToList pkgs.none.icecap.configured (_: configured: [
+      configured.sysroot-rs
+    ]))
+
+    (map (lib.mapAttrsToList (_: plat: plat.run)) [
+      meta.tests.realm-vm
+      meta.tests.analysis
+      meta.tests.benchmark-utilisation
+    ])
+
+    meta.tests.firecracker.rpi4.boot
+
+  ];
 
 in {
 
