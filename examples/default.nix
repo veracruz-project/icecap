c78f8ddbef551337cff9739f89f25e41cc428c6d
Nick Spinale <nick@nickspinale.com>
2022-01-18 19:39:24 +0000

    docs: Rename dynamism demo

--- examples/default.nix
+++ examples/default.nix
@@ -9,8 +9,8 @@ let
     minimal-capdl = ./03-minimal-capdl;
     minimal-capdl-with-rust = ./04-minimal-capdl-with-rust;
     basic-system = ./05-basic-system;
-    dyndl = ./wip-dyndl;
-    hypervisor = ./06-hypervisor;
+    dynamism = ./06-dynamism;
+    hypervisor = ./07-hypervisor;
   };
 
 in lib.flip lib.mapAttrs examples (_: path:
