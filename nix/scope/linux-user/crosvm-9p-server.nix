6c3aa40e370004952a20199d7211d94ed8b5f591
Nick Spinale <nick@nickspinale.com>
2022-01-21 15:12:52 +0000

    9p: Remove unecessary dependencies for server

--- nix/scope/linux-user/crosvm-9p-server.nix
+++ nix/scope/linux-user/crosvm-9p-server.nix
@@ -1,16 +1,10 @@
 { buildRustPackageIncrementally, globalCrates
-, pkgconfig, dbus
 }:
 
 buildRustPackageIncrementally rec {
   rootCrate = globalCrates.crosvm-9p-server-cli;
 
   extra = attrs: {
-    nativeBuildInputs = (attrs.nativeBuildInputs or []) ++ [ pkgconfig ];
-    buildInputs = (attrs.buildInputs or []) ++ [ dbus ];
-
-    PKG_CONFIG_ALLOW_CROSS = 1;
-
     passthru = (attrs.passthru or {}) // {
       exe = rootCrate.name;
     };
