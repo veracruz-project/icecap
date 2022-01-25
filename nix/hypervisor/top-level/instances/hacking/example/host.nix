c1104de912553a7f8f57d9400c01f0d1c5a1b8a9
Nick Spinale <nick@nickspinale.com>
2022-01-25 12:22:08 +0000

    instances: Add start_realm to hacking example

--- nix/hypervisor/top-level/instances/hacking/example/host.nix
+++ nix/hypervisor/top-level/instances/hacking/example/host.nix
@@ -20,14 +20,22 @@ in {
         copy_bin_and_libs ${pkgs.strace}/bin/strace
       '';
 
-      initramfs.extraInitCommands = ''
+      initramfs.extraInitCommands = mkAfter ''
         echo 2 > /proc/sys/kernel/randomize_va_space
         ulimit -c unlimited
+
+        export realm_affinity=0x2
       '';
     }
 
     {
       initramfs.profile = ''
+        start_realm() {
+          echo "starting realm"
+          time taskset $realm_affinity icecap-host create 0 /spec.bin && \
+            chrt -b 0 taskset $realm_affinity icecap-host run 0 0
+        }
+
         s() {
           while true; do sha256sum /mnt/${testBigFile}; done
         }
