3e4de90967e221f792e05ededd2609a4cf6d738d
Nick Spinale <nick@nickspinale.com>
2022-01-26 14:13:32 +0000

    nix: Clean up "everything" attrsets

--- nix/default.nix
+++ nix/default.nix
@@ -1,6 +1,43 @@
-let
+rec {
   framework = import ./framework;
-  hypervisor = import ./hypervisor { inherit framework; };
-in framework.lib.fix (self: {
-  inherit framework hypervisor;
-} // import ./top-level self)
+
+  hypervisor = import ./hypervisor {
+    inherit framework;
+  };
+
+  examples = import ../examples {
+    inherit framework hypervisor;
+  };
+
+  demos = {
+    hypervisor-demo = import ../demos/hypervisor-demo {
+      inherit hypervisor;
+    };
+  };
+
+  everything =
+    let
+      inherit (framework) lib;
+      mk = name: drvs: framework.pkgs.dev.writeText name (toString (lib.flatten drvs));
+    in rec {
+      cached = mk "everything-cached" [
+        framework.everything.cached
+        hypervisor.everything.cached
+        (lib.mapAttrsToList (_: lib.mapAttrsToList (_: plat: plat.run)) demos)
+        (lib.mapAttrsToList (_: example: example.run) examples)
+      ];
+      pure = mk "everything-pure" [
+        cached
+        framework.everything.pure
+        hypervisor.everything.pure
+      ];
+      impure = mk "everything-impure" [
+        framework.everything.impure
+      ];
+      all = mk "everything-all" [
+        pure
+        impure
+      ];
+    };
+
+}
--- nix/top-level/default.nix
+++ nix/top-level/default.nix
@@ -5,39 +5,8 @@ let
 
 in {
 
-  examples = import ../../examples {
-    inherit framework hypervisor;
-  };
-  demos = {
-    hypervisor-demo = import ../../demos/hypervisor-demo {
-      inherit hypervisor;
-    };
-  };
 
-  everything =
-    let
-      combine = attr: [
-        framework.everything.${attr}
-        hypervisor.everything.${attr}
-      ];
-      mk = name: drvs: framework.pkgs.dev.writeText name (toString (lib.flatten drvs));
-    in rec {
-      cached = mk "everything-cached" [
-        (combine "cached")
-        (lib.mapAttrsToList (_: lib.mapAttrsToList (_: plat: plat.run)) demos)
-        (lib.mapAttrsToList (_: example: example.run) examples)
-      ];
-      pure = mk "everything-pure" [
-        cached
-        (combine "pure")
-      ];
-      impure = mk "everything-impure" [
-        (combine "impure")
-      ];
-      all = mk "everything-all" [
-        pure
-        impure
-      ];
-    };
+
+
 
 }
