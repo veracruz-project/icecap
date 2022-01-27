615a221d27834293de86eff1f0e2c3ee3adb6dcc
Nick Spinale <nick@nickspinale.com>
2022-01-27 03:29:17 +0000

    docs: Fix shortcuts example

--- hack/dev-environment-examples/shortcuts.example.nix
+++ hack/dev-environment-examples/shortcuts.example.nix
@@ -1,17 +1,17 @@
 let
-  topLevel = import ../.;
+  topLevel = import ../../.;
 
-in topLevel.lib.fix (self: topLevel // (with self; {
+in topLevel.framework.lib.fix (self: topLevel // (with self; {
 
-  hh = meta.instances.hacking.example;
+  hh = hypervisor.instances.hacking.example;
   hv = hh.virt;
   hr = hh.rpi4;
   v = hv.run;
   vc = hv.configured;
   rb = hr.run.boot;
 
-  lhv = pkgs.linux.icecap.linuxKernel.host.virt;
-  lhvc = pkgs.linux.icecap.linuxKernel.host.virt.configEnv;
+  lhv = framework.pkgs.linux.icecap.linuxKernel.host.virt;
+  lhvc = framework.pkgs.linux.icecap.linuxKernel.host.virt.configEnv;
 
   sr = vc.sysroot-rs;
   sre = vc.sysroot-rs.env;
