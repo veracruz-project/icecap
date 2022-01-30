a15f43c4affe743d98ae49176da36a161ab822ad
Nick Spinale <nick@nickspinale.com>
2022-01-30 17:57:16 +0000

    nix: Fix local source path in config.nix

--- nix/framework/config.nix
+++ nix/framework/config.nix
@@ -2,6 +2,6 @@
   source = {
     gitUrlOf = repo: "https://gitlab.com/arm-research/security/icecap/${repo}";
     keepRefOf = rev: "refs/tags/icecap/keep/${builtins.substring 0 32 rev}";
-    localPathOf = repo: ../../../../local + "/${repo}";
+    localPathOf = repo: ../../../local + "/${repo}";
   };
 }
