34a97f030b6fc083cb9aaea5c6c7f03e1e618f45
Nick Spinale <nick@nickspinale.com>
2022-01-31 01:21:49 +0000

    rust: Remove icecap-unwind crate source

--- src/rust/crates/framework/base/icecap-unwind/src/arch/mod.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(target_arch = "aarch64")]
-pub mod aarch64;
-
-#[cfg(target_arch = "aarch64")]
-pub use aarch64::{
-    Unwinder, StackFrames, StackFrame,
-    DwarfUnwinder,
-};
--- src/rust/crates/framework/base/icecap-unwind/src/sys/mod.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(target_os = "icecap")]
-mod icecap;
-
-#[cfg(target_os = "icecap")]
-pub(crate) use icecap::find_cfi_sections;
