a40c434de3037e2d1715386bf9e4c95920506497
Jim Posen <jim.posen@gmail.com>
2022-02-06 13:55:57 -0700

    icecap-rpc: Modify RPC interface to use IPCBuffer
    This makes RPC calls memory-safe by accessing the thread-local
    IPCBuffer instead of MessageRegister.

--- src/rust/crates/framework/base/icecap-rpc/src/lib.rs
+++ src/rust/crates/framework/base/icecap-rpc/src/lib.rs
@@ -2,33 +2,37 @@
 
 extern crate alloc;
 
-use alloc::vec::Vec;
 use core::marker::PhantomData;
 
 use icecap_sel4::prelude::*;
 
 pub use icecap_rpc_types::{self as types, *};
 
-struct ReceivingImpl {
+struct ReceivingImpl<'a> {
     length: usize,
     cursor: usize,
+    ipcbuf: &'a IPCBuffer,
 }
 
-impl ReceivingImpl {
-    fn new(length: usize) -> Self {
-        Self { length, cursor: 0 }
+impl<'a> ReceivingImpl<'a> {
+    fn new(ipcbuf: &'a IPCBuffer, length: usize) -> Self {
+        Self {
+            ipcbuf,
+            length,
+            cursor: 0,
+        }
     }
 
-    fn complete<T: RPC>(info: &MessageInfo) -> T {
-        let mut receiving = Self::new(info.length() as usize);
+    fn complete<T: RPC>(ipcbuf: &IPCBuffer, info: MessageInfo) -> T {
+        let mut receiving = ReceivingImpl::new(ipcbuf, info.length() as usize);
         T::recv(&mut receiving)
     }
 }
 
-impl Receiving for ReceivingImpl {
+impl<'a> Receiving for ReceivingImpl<'a> {
     fn read_value(&mut self) -> ParameterValue {
         assert_ne!(self.cursor, self.length);
-        let value = MessageRegister::new(self.cursor as i32).get();
+        let value = self.ipcbuf.msg_regs()[self.cursor];
         self.cursor += 1;
         value
     }
@@ -38,27 +42,50 @@ impl Receiving for ReceivingImpl {
     }
 }
 
-struct SendingImpl {
+struct SendingImpl<'a> {
     cursor: usize,
+    ipcbuf: &'a mut IPCBuffer,
 }
 
-impl SendingImpl {
-    fn new() -> Self {
-        Self { cursor: 0 }
+impl<'a> SendingImpl<'a> {
+    fn new(ipcbuf: &'a mut IPCBuffer) -> Self {
+        Self { cursor: 0, ipcbuf }
     }
 
-    fn complete(message: &impl RPC) -> MessageInfo {
-        let mut sending = SendingImpl::new();
+    fn complete(ipcbuf: &mut IPCBuffer, message: &impl RPC) -> MessageInfo {
+        let mut sending = SendingImpl::new(ipcbuf);
         message.send(&mut sending);
         let length = sending.cursor;
         MessageInfo::new(0, 0, 0, length as u64)
     }
 }
 
-impl Sending for SendingImpl {
+impl<'a> Sending for SendingImpl<'a> {
     fn write_value(&mut self, value: ParameterValue) {
-        MessageRegister::new(self.cursor as i32).set(value);
+        self.ipcbuf.msg_regs_mut()[self.cursor] = value;
         self.cursor += 1;
+    }
+}
+
+pub struct ReceivingIPC<'a> {
+    pub ipcbuf: &'a IPCBuffer,
+    pub info: MessageInfo,
+    pub badge: Badge,
+    receiving: ReceivingImpl<'a>,
+}
+
+impl<'a> ReceivingIPC<'a> {
+    fn new(ipcbuf: &'a IPCBuffer, info: MessageInfo, badge: Badge) -> Self {
+        Self {
+            ipcbuf,
+            info,
+            badge,
+            receiving: ReceivingImpl::new(ipcbuf, info.length() as usize),
+        }
+    }
+
+    pub fn read<Output: RPC>(&mut self) -> Output {
+        Output::recv(&mut self.receiving)
     }
 }
 
@@ -79,52 +106,76 @@ impl<Input> Client<Input> {
 
 impl<Input: RPC> Client<Input> {
     pub fn send(&self, input: &Input) {
-        self.endpoint.send(SendingImpl::complete(input))
+        IPCBuffer::with_mut(|ipcbuf| self.send_with_ipcbuf(ipcbuf, input))
+    }
+
+    pub fn send_with_ipcbuf(&self, ipcbuf: &mut IPCBuffer, input: &Input) {
+        let info = SendingImpl::complete(ipcbuf, input);
+        self.endpoint.send(info);
     }
 
     pub fn call<Output: RPC>(&self, input: &Input) -> Output {
-        ReceivingImpl::complete(&self.endpoint.call(SendingImpl::complete(input)))
+        IPCBuffer::with_mut(|ipcbuf| self.call_with_ipcbuf(ipcbuf, input))
+    }
+
+    pub fn call_with_ipcbuf<Output: RPC>(&self, ipcbuf: &mut IPCBuffer, input: &Input) -> Output {
+        let send_info = SendingImpl::complete(ipcbuf, input);
+        let recv_info = self.endpoint.call(send_info);
+        ReceivingImpl::complete(ipcbuf, recv_info)
     }
 }
 
 pub mod server {
     use super::*;
 
-    pub fn prepare<Output: RPC>(output: &Output) -> MessageInfo {
-        SendingImpl::complete(output)
+    pub fn recv<F, T>(endpoint: Endpoint, f: F) -> T
+    where
+        F: FnOnce(ReceivingIPC) -> T,
+    {
+        IPCBuffer::with_mut(|ipcbuf| recv_with_ipcbuf(ipcbuf, endpoint, f))
     }
 
-    pub fn recv<Input: RPC>(info: &MessageInfo) -> Input {
-        ReceivingImpl::complete(info)
+    pub fn recv_with_ipcbuf<F, T>(ipcbuf: &mut IPCBuffer, endpoint: Endpoint, f: F) -> T
+    where
+        F: FnOnce(ReceivingIPC) -> T,
+    {
+        let (info, badge) = endpoint.recv();
+        let receiving = ReceivingIPC::new(ipcbuf, info, badge);
+        f(receiving)
     }
 
     pub fn send<Output: RPC>(endpoint: Endpoint, output: &Output) {
-        endpoint.send(SendingImpl::complete(output))
+        IPCBuffer::with_mut(|ipcbuf| send_with_ipcbuf(ipcbuf, endpoint, output))
+    }
+
+    pub fn send_with_ipcbuf<Output: RPC>(
+        ipcbuf: &mut IPCBuffer,
+        endpoint: Endpoint,
+        output: &Output,
+    ) {
+        let info = SendingImpl::complete(ipcbuf, output);
+        endpoint.send(info)
     }
 
     pub fn reply<Output: RPC>(output: &Output) {
-        sel4::reply(SendingImpl::complete(output))
+        IPCBuffer::with_mut(|ipcbuf| reply_with_ipcbuf(ipcbuf, output))
+    }
+
+    pub fn reply_with_ipcbuf<Output: RPC>(ipcbuf: &mut IPCBuffer, output: &Output) {
+        let info = SendingImpl::complete(ipcbuf, output);
+        sel4::reply(info);
     }
 }
 
 pub mod proxy {
     use super::*;
 
-    pub fn down(info: &MessageInfo) -> Vec<ParameterValue> {
-        let mut parameters = Vec::new();
-        let length = info.length() as usize;
-        let mut receiving = ReceivingImpl::new(length);
-        for _ in 0..length {
-            parameters.push(receiving.read());
-        }
-        parameters
+    pub fn down(ipcbuf: &IPCBuffer, info: MessageInfo) -> &[u64] {
+        &ipcbuf.msg_regs()[..info.length() as usize]
     }
 
-    pub fn up(parameters: &[ParameterValue]) -> MessageInfo {
-        let mut sending = SendingImpl::new();
-        for parameter in parameters {
-            sending.write(*parameter);
-        }
+    pub fn up(ipcbuf: &mut IPCBuffer, parameters: &[u64]) -> MessageInfo {
+        ipcbuf.msg_regs_mut()[..parameters.len()].copy_from_slice(parameters);
         MessageInfo::new(0, 0, 0, parameters.len() as u64)
     }
 }
