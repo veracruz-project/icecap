9bc6ba0a7ba49f253af14ddc2356dab91a6fc962
Eric Van Hensbergen <ericvh@gmail.com>
2022-02-10 17:57:13 +0000

    Copy of sel4test simple cmake config, but largely adding as a placeholder for the manifest.
    Signed-off-by: Eric Van Hensbergen <eric.vanhensbergen@arm.com>

---
+++ easy-settings.cmake
@@ -0,0 +1,35 @@
+#
+# Copyright 2019, Data61, CSIRO (ABN 41 687 119 230)
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+# Define our top level settings.  Whilst they have doc strings for readability
+# here, they are hidden in the cmake-gui as they cannot be reliably changed
+# after the initial configuration.  Enterprising users can still change them if
+# they know what they are doing through advanced mode.
+#
+# Users should initialize a build directory by doing something like:
+#
+# mkdir build_sabre
+# cd build_sabre
+#
+# Then
+#
+# ../griddle --PLATFORM=sabre --SIMULATION
+# ninja
+#
+set(SIMULATION OFF CACHE BOOL "Include only simulation compatible tests")
+set(RELEASE OFF CACHE BOOL "Performance optimized build")
+set(VERIFICATION OFF CACHE BOOL "Only verification friendly kernel features")
+set(BAMBOO OFF CACHE BOOL "Enable machine parseable output")
+set(DOMAINS OFF CACHE BOOL "Test multiple domains")
+set(SMP OFF CACHE BOOL "(if supported) Test SMP kernel")
+set(NUM_NODES "" CACHE STRING "(if SMP) the number of nodes (default 4)")
+set(PLATFORM "aarch64" CACHE STRING "Platform to test")
+set(ARM_HYP OFF CACHE BOOL "Hyp mode for ARM platforms")
+set(MCS OFF CACHE BOOL "MCS kernel")
+set(KernelSel4Arch "" CACHE STRING "aarch32, aarch64, arm_hyp, ia32, x86_64, riscv32, riscv64")
+set(LibSel4TestPrinterRegex ".*" CACHE STRING "A POSIX regex pattern used to filter tests")
+set(LibSel4TestPrinterHaltOnTestFailure OFF CACHE BOOL "Halt on the first test failure")
+mark_as_advanced(CLEAR LibSel4TestPrinterRegex LibSel4TestPrinterHaltOnTestFailure)
