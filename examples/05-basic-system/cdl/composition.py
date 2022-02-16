d70d2faa98d9ed8a9a3e54d215a8e616ccd30e2a
Mathias Brossard <mathias.brossard@arm.com>
2022-02-16 14:22:12 -0600

    Get 05-basic-system compiling with Makefile

--- examples/05-basic-system/cdl/composition.py
+++ examples/05-basic-system/cdl/composition.py
@@ -1,3 +1,7 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import os
+
 from icecap_framework import BaseComposition
 
 from serial_server import SerialServer
@@ -11,4 +15,42 @@ class Composition(BaseComposition):
         self.timer_server = self.component(TimerServer, 'timer_server')
         self.component(Application, 'application')
 
-Composition.from_env().run()
+parser = ArgumentParser()
+parser.add_argument('-c', '--components', metavar='COMPONENTS', type=Path)
+parser.add_argument('-o', '--out-dir', metavar='OUT_DIR', type=Path)
+parser.add_argument('-s', '--object-sizes', metavar='OBJECT_SIZES', type=Path)
+args = parser.parse_args()
+
+components_path = os.path.abspath(args.components)
+
+config = {
+    "plat": "virt",
+    "num_cores": 4,
+    "num_realms": 2,
+    "default_affinity": 1,
+    "hack_realm_affinity": 1,
+    "object_sizes": args.object_sizes,
+    "components": {
+        "application": {
+            "image": {
+                "full": os.path.join(components_path, "application.full.elf"),
+                "min": os.path.join(components_path, "application.min.elf"),
+            }
+        },
+        "serial_server": {
+            "image": {
+                "full": os.path.join(components_path, "serial-server.full.elf"),
+                "min": os.path.join(components_path, "serial-server.min.elf"),
+            }
+
+        },
+        "timer_server": {
+            "image": {
+                "full": os.path.join(components_path, "timer-server.full.elf"),
+                "min": os.path.join(components_path, "timer-server.min.elf"),
+            }
+        },
+    }
+}
+
+Composition(args.out_dir, config).run()
